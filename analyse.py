a0857db4ded392ebff3d32c5bd14b1b8fc0dd1a1
Nicolas Hug <nicolas.hug@irit.fr>
2015-03-08 15:47:27 +0000

    using a more convenient way to filter estimation

--- analyse.py
+++ analyse.py
@@ -29,46 +29,52 @@ for k, v in infos['params'].items():
 print('-' * 10)
 print('-' * 10)
 
-def err(est):
+def err(e):
     """return the error between the expected rating and the estimated one"""
-    return est['est'] - est['r0']
+    return e['est'] - e['r0']
 
-def details(est):
+def details(e):
     """print details on an estimation"""
     # ids, true rating, etimation and error
     print("u0: {0:<3d}    m0: {1:<4d}   r0: {2}   est: {3:1.2f}"
-        "   err: {4:-2.2f}".format(est['u0'], est['m0'], est['r0'], est['est'],
-        err(est)))
+        "   err: {4:-2.2f}".format(e['u0'], e['m0'], e['r0'], e['est'],
+        err(e)))
     # u0 ratings mean and count
     print("u0 ratings:")
-    print("\tcount: {0:d}".format(len(infos['ur'][est['u0']])))
-    print("\tmean : {0:1.4f}".format(np.mean(infos['ur'][est['u0']], 0)[1]))
+    print("\tcount: {0:d}".format(len(infos['ur'][e['u0']])))
+    print("\tmean : {0:1.4f}".format(np.mean(infos['ur'][e['u0']], 0)[1]))
     # m0 ratings mean and count
     print("m0 ratings:")
-    print("\tcount: {0:d}".format(len(infos['mr'][est['m0']])))
-    if infos['mr'][est['m0']]:
-        print("\tmean : {0:1.4f}".format(np.mean(infos['mr'][est['m0']], 0)[1]))
+    print("\tcount: {0:d}".format(len(infos['mr'][e['m0']])))
+    if infos['mr'][e['m0']]:
+        print("\tmean : {0:1.4f}".format(np.mean(infos['mr'][e['m0']], 0)[1]))
     else:
         print("\tmean : no f***ing way!")
         
 
-def filterByError(ests, inf=0., sup=4.):
-    """return an iterator with all the estimations where abs(error) is between
-    inf and sup (both included)"""
+def errorBetween(e, inf=0., sup=4.):
+    """return true if abs(err) is between inf and sup (both included)"""
+    return inf <= abs(err(e)) <= sup
 
-    return filter(lambda x: (inf <= abs(err(x)) <= sup), ests)
-
-def filterByRatingsCount(ests, x='u', inf=0, sup=float('inf')):
-    """return an iterator with all the estimations where number of ratings for
-    x ('u' or 'm') is between inf and sup (both included)"""
-
+def ratingsCountBetween(e, x='u', inf=0, sup=float('inf')):
+    """return true if the number of rating for x ('u' or 'm') is between inf
+    and sup (both included)"""
     xr = infos['ur'] if x == 'u' else infos['mr']
     x0 = 'u0' if x == 'u' else 'm0'
+    return inf <= len(xr[e[x0]]) <= sup
 
-    return filter(lambda e: inf <= len(xr[e[x0]]) <= sup, ests)
+# requirements that an estimation needs to fit
+requirements = (lambda e: 
+    errorBetween(e, inf=3.5)  and 
+    ratingsCountBetween(e, x='m', sup=100))
+# in example above, we look for all the estimations where abs(error) is more
+# than 3.5 and where m0 has been rated less than 100 times.
 
-# detail of predictions
-for est in filterByError(infos['ests'], inf=3.5):
-#for est in filterByError(filterByRatingsCount(infos['ests'], x='m', sup=100), inf=3.5):
-    details(est)
+# iterator with all estimations fitting the previously defined requirements
+interstingEstimations = filter(requirements, infos['ests'])
+
+
+# print details for estimations we are interested in
+for e in interstingEstimations:
+    details(e)
     print('-' * 52)
