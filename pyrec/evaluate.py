09aec6dc0a9b23a3b83fb615ab79c788804357df
Nicolas Hug <nh.nicolas.hug@gmail.com>
2016-01-21 17:18:12 +0100

    added FCP calculation

--- pyrec/evaluate.py
+++ pyrec/evaluate.py
@@ -4,15 +4,18 @@ import numpy as np
 def evaluate(algo, data):
     rmses = []
     maes = []
+    fcps = []
     for fold_i, (trainset, testset) in enumerate(data.folds):
         print('-' * 20)
         print('fold ' + str(fold_i))
         algo.train(trainset)
         algo.test(testset)
-        rmse, mae = stats.compute_stats(algo.preds)
+        rmse, mae = stats.get_rmse_mae(algo.preds)
         rmses.append(rmse)
         maes.append(mae)
+        fcps.append(stats.get_FCP(algo.preds))
 
     print('-' * 20)
     print('mean RMSE: {0:1.4f}'.format(np.mean(rmses)))
     print('mean MAE : {0:1.4f}'.format(np.mean(maes)))
+    print('mean FCP : {0:1.4f}'.format(np.mean(fcps)))
--- pyrec/stats.py
+++ pyrec/stats.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def compute_stats(preds, output=True):
+def get_rmse_mae(preds, output=True):
     """compute some statistics (RMSE, coverage...) on a list of predictions"""
 
     nImp = 0
@@ -22,3 +22,19 @@ def compute_stats(preds, output=True):
         print('MAE: {0:1.4f}'.format(mae))
 
     return rmse, mae
+
+def get_FCP(preds, output=True):
+
+    nc = 0
+    nd = 0
+    for _, _, r0i, esti, _ in preds:
+        for _, _, r0j, estj, _ in preds:
+            if esti > estj and r0i > r0j:
+                nc += 1
+            if esti >= estj and r0i < r0j:
+                nd += 1
+
+    fcp = nc / (nc + nd)
+    print('FCP: {0:1.4f}'.format(fcp))
+
+    return fcp
