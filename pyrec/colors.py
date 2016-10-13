f974097e3d9b5c55d995f14d60cbb619f17e9f99
Nicolas Hug <nh.nicolas.hug@gmail.com>
2016-10-13 18:47:44 +0200

    sourced colors.py

--- pyrec/colors.py
+++ pyrec/colors.py
@@ -1,4 +1,6 @@
-"""A class for adding color in the term output"""
+"""A module for colored terminal outputs"""
+
+
 HEADER = '\033[95m'
 OKBLUE = '\033[94m'
 OKGREEN = '\033[92m'
@@ -7,3 +9,6 @@ FAIL = '\033[91m'
 ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
+
+# Source :
+# http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
