1e5d2d03318428a097e4ddc5b8775c87edfcc906
Nicolas Hug <nh.nicolas.hug@gmail.com>
2016-10-17 14:05:10 +0200

    created two small ratings file (train/test) for testing purposes

--- test/test_algorithms.py
+++ test/test_algorithms.py
@@ -1,18 +1,21 @@
+import os
+
 from pyrec.prediction_algorithms import *
 from pyrec.dataset import Dataset
 from pyrec.dataset import Reader
 from pyrec.evaluate import evaluate
 
+
 algorithms = (NormalPredictor, BaselineOnly, KNNBasic, KNNWithMeans,
               KNNBaseline)
 
-train_file = 'datasets/ml-100k/ml-100k/u1.base'
-test_file = 'datasets/ml-100k/ml-100k/u1.test'
+# the test and train files are from the ml-100k dataset (10% of u1.base and
+# 10 % of u1.test)
+train_file = os.path.join(os.path.dirname(__file__), './u1_ml100k_train')
+test_file = os.path.join(os.path.dirname(__file__), './u1_ml100k_test')
 data = Dataset.load_from_files(train_file, test_file, Reader('ml-100k'))
 
 def test_algos():
     for klass in algorithms:
         algo = klass()
         evaluate(algo, data)
-
-
