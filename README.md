668f5045c192f4bd8f14cd521c7a97126d4373cd
Nicolas Hug <nh.nicolas.hug@gmail.com>
2016-10-23 17:02:59 +0200

    Added all needed files for PyPI packaging

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 RecSys
 ======
 
+version number: 0.0.1
+author: Nicolas Hug
+
+Overview
+--------
+
 RecSys is an open source Python package that provides with tools to build and
 evaluate the performance of many recommender system prediction algorithms. Its
 goal is to make life easy(-ier) for reseachers and students who want to play
@@ -10,20 +16,20 @@ A strong emphasis is laid on documentation, which we have tried to make as
 clear and precise as possible by pointing out every details of the algorithms,
 in order for the practitioner to have perfect control over his experiments.
 
+Installation / Usage
+--------------------
 
-Dependecies
------------
+To install use pip:
 
-To run RecSys, you'll need:
+    $ pip install recsys
 
-- Python 3
-- Numpy
-- Cython
 
-And if you intend to use compare tools:
+Or clone the repo:
 
-- Pandas
-- Matplotlib
-- Ipython
+    $ git clone https://github.com/Niourf/recsys.git
+    $ python setup.py install
 
-We use pytest for testing
+Example
+-------
+
+TBD
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+__version__ = '0.0.2'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+
+setup(
+    name='recsys',
+    version=__version__,
+    description=('A recommender system package aimed towards researchers ' +
+                 'and students.'),
+    long_description=long_description,
+    url='https://github.com/Niourf/recsys',
+    download_url='https://github.com/Niourf/recsys/tarball/' + __version__,
+    license='GPLv3+',
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Intended Audience :: Education',
+      'Intended Audience :: Science/Research',
+      'Topic :: Scientific/Engineering',
+      'License :: OSI Approved',
+      'Programming Language :: Python :: 3',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    author='Nicolas Hug',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    author_email='nh.nicolas.hug@gmail.com'
+)
