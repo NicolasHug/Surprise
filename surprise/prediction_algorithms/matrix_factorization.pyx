ffc4efb57e5a25f5642dc84a66d1dbb4530078ee
Nicolas Hug <nh.nicolas.hug@gmail.com>
2016-12-30 15:51:00 +0100

    Added a biased version for NMF

--- surprise/prediction_algorithms/matrix_factorization.pyx
+++ surprise/prediction_algorithms/matrix_factorization.pyx
@@ -234,6 +234,8 @@ class SVD(AlgoBase):
 
         if self.trainset.knows_user(u) and self.trainset.knows_item(i):
             est += np.dot(self.qi[i], self.pu[u])
+        else:
+            raise PredictionImpossible
 
         return est
 
@@ -464,14 +466,35 @@ class NMF(AlgoBase):
     are uniformly initialized between ``init_low`` and ``init_high``. Change
     them at your own risks!
 
+    A biased version is available by setting the ``biased`` parameter to
+    ``True``. In this case, the prediction is set as
+
+    .. math::
+        \hat{r}_{ui} = \mu + b_u + b_i + q_i^Tp_u,
+
+    still ensuring positive factors. Baselines are optimized in the same way as
+    in the :class:`SVD` algorithm. While yielding better accuracy, the biased
+    version seems highly prone to overfitting so you may want to reduce the
+    number of factors (or increase regularization).
+
     Args:
         n_factors: The number of factors. Default is ``15``.
         n_epochs: The number of iteration of the SGD procedure. Default is
             ``50``.
-        reg_u: The regularization term for users :math:`\lambda_u`. Default is
+        biased(bool): Whether to use baselines (or biases). Default is
+            ``False``.
+        reg_pu: The regularization term for users :math:`\lambda_u`. Default is
             ``0.06``.
-        reg_i: The regularization term for items :math:`\lambda_i`. Default is
+        reg_qi: The regularization term for items :math:`\lambda_i`. Default is
             ``0.06``.
+        reg_bu: The regularization term for :math:`b_u`. Only relevent for
+            biased version. Default is ``0.02``.
+        reg_bi: The regularization term for :math:`b_i`. Only relevent for
+            biased version. Default is ``0.02``.
+        lr_bu: The learning rate for :math:`b_u`. Only relevent for biased
+            version. Default is ``0.005``.
+        lr_bi: The learning rate for :math:`b_i`. Only relevent for biased
+            version. Default is ``0.005``.
         init_low: Lower bound for random initialization of factors. Must be
             greater than ``0`` to ensure non-negative factors. Default is
             ``0``.
@@ -480,13 +503,19 @@ class NMF(AlgoBase):
         verbose: If ``True``, prints the current epoch. Default is ``False``.
     """
 
-    def __init__(self, n_factors=15, n_epochs=50, reg_u=.06, reg_i=.06,
+    def __init__(self, n_factors=15, n_epochs=50, biased=False, reg_pu=.06,
+                 reg_qi=.06, reg_bu=.02, reg_bi=.02, lr_bu=.005, lr_bi=.005,
                  init_low=0, init_high=1, verbose=False):
 
         self.n_factors = n_factors
         self.n_epochs = n_epochs
-        self.reg_u = reg_u
-        self.reg_i = reg_i
+        self.biased = biased
+        self.reg_pu = reg_pu
+        self.reg_qi = reg_qi
+        self.lr_bu = lr_bu
+        self.lr_bi = lr_bi
+        self.reg_bu = reg_bu
+        self.reg_bi = reg_bi
         self.init_low = init_low
         self.init_high = init_high
         self.verbose = verbose
@@ -507,6 +536,10 @@ class NMF(AlgoBase):
         cdef np.ndarray[np.double_t, ndim=2] pu
         cdef np.ndarray[np.double_t, ndim=2] qi
 
+        # user and item biases
+        cdef np.ndarray[np.double_t] bu
+        cdef np.ndarray[np.double_t] bi
+
         # auxiliary matrices used in optimization process
         cdef np.ndarray[np.double_t, ndim=2] user_num
         cdef np.ndarray[np.double_t, ndim=2] user_denom
@@ -514,9 +547,14 @@ class NMF(AlgoBase):
         cdef np.ndarray[np.double_t, ndim=2] item_denom
 
         cdef int u, i, f
-        cdef double r, est, l
-        cdef double reg_u = self.reg_u
-        cdef double reg_i = self.reg_i
+        cdef double r, est, l, dot, err
+        cdef double reg_pu = self.reg_pu
+        cdef double reg_qi = self.reg_qi
+        cdef double reg_bu = self.reg_bu
+        cdef double reg_bi = self.reg_bi
+        cdef double lr_bu = self.lr_bu
+        cdef double lr_bi = self.lr_bi
+        cdef double global_mean = self.trainset.global_mean
 
         # Randomly initialize user and item factors
         pu = np.random.uniform(self.init_low, self.init_high,
@@ -524,11 +562,18 @@ class NMF(AlgoBase):
         qi = np.random.uniform(self.init_low, self.init_high,
                                size=(trainset.n_items, self.n_factors))
 
+        bu = np.zeros(trainset.n_users, np.double)
+        bi = np.zeros(trainset.n_items, np.double)
+
+        if not self.biased:
+            global_mean = 0
+
         for current_epoch in range(self.n_epochs):
 
             if self.verbose:
                 print("Processing epoch {}".format(current_epoch))
 
+            # (re)initialize nums and denoms to zero
             user_num = np.zeros((trainset.n_users, self.n_factors))
             user_denom = np.zeros((trainset.n_users, self.n_factors))
             item_num = np.zeros((trainset.n_items, self.n_factors))
@@ -537,10 +582,19 @@ class NMF(AlgoBase):
             # Compute numerators and denominators for users and items factors
             for u, i, r in trainset.all_ratings():
 
-                est = 0  # <q_i, p_u>
+                # compute current estimation and error
+                dot = 0  # <q_i, p_u>
                 for f in range(self.n_factors):
-                    est += qi[i, f] * pu[u, f]
+                    dot += qi[i, f] * pu[u, f]
+                est = global_mean + bu[u] + bi[i] + dot
+                err = r - est
 
+                # update biases
+                if self.biased:
+                    bu[u] += lr_bu * (err - reg_bu * bu[u])
+                    bi[i] += lr_bi * (err - reg_bi * bi[i])
+
+                # compute numerators and denominators
                 for f in range(self.n_factors):
                     user_num[u, f] += qi[i, f] * r
                     user_denom[u, f] += qi[i, f] * est
@@ -551,23 +605,33 @@ class NMF(AlgoBase):
             for u in trainset.all_users():
                 l = len(trainset.ur[u])
                 for f in range(self.n_factors):
-                    user_denom[u, f] += l * reg_u * pu[u, f]
+                    user_denom[u, f] += l * reg_pu * pu[u, f]
                     pu[u, f] *= user_num[u, f] / user_denom[u, f]
 
             # Update item factors
             for i in trainset.all_items():
                 l = len(trainset.ir[i])
                 for f in range(self.n_factors):
-                    item_denom[i, f] += l * reg_i * qi[i, f]
+                    item_denom[i, f] += l * reg_qi * qi[i, f]
                     qi[i, f] *= item_num[i, f] / item_denom[i, f]
 
+        self.bu = bu
+        self.bi = bi
         self.pu = pu
         self.qi = qi
 
     def estimate(self, u, i):
 
+        est = self.trainset.global_mean if self.biased else 0
+
+        if self.trainset.knows_user(u):
+            est += self.bu[u]
+
+        if self.trainset.knows_item(i):
+            est += self.bi[i]
+
         if self.trainset.knows_user(u) and self.trainset.knows_item(i):
-            est = np.dot(self.qi[i], self.pu[u])
+            est += np.dot(self.qi[i], self.pu[u])
         else:
             raise PredictionImpossible
 
