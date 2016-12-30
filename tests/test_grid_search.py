04188780be6191172d05ae584736f8aacb2cb092
mahermalaeb <m.n.malaeb@gmail.com>
2016-12-30 09:48:38 +0200

    Updated GridSearch evaluate():  - Added best score, best parameters and best index attributes  - Removed 1 attribute and added cv_results_  attribute analogous to sklearn implementation  - Added tests for best attributes for RMSE and FCP measures

--- tests/test_grid_search.py
+++ tests/test_grid_search.py
@@ -18,8 +18,24 @@ train_file = os.path.join(os.path.dirname(__file__), './u1_ml100k_train')
 test_file = os.path.join(os.path.dirname(__file__), './u1_ml100k_test')
 data = Dataset.load_from_folds([(train_file, test_file)], Reader('ml-100k'))
 
-def test_grid_search():
+def test_grid_search_cv_results():
     param_grid = {'n_epochs':[5,10], 'lr_all': [0.002, 0.005], 'reg_all': [0.4,0.6]}
     gridSearch = GridSearch(SVD,param_grid)
     gridSearch.evaluate(data)
-    assert len(gridSearch.combination_score_list) == 8
+    assert len(gridSearch.cv_results_['params']) == 8
+
+def test_best_rmse():
+    param_grid = {'n_epochs': [5, 10], 'lr_all': [0.002, 0.005], 'reg_all': [0.4, 0.6]}
+    gridSearch = GridSearch(SVD,param_grid)
+    gridSearch.evaluate(data)
+    assert gridSearch.best_index_ == 7
+    assert gridSearch.best_params_ == {u'lr_all': 0.005, u'reg_all': 0.6, u'n_epochs': 10}
+    assert (gridSearch.best_score_ - 1.06880357239) < 0.000001 # scores are equal
+
+def test_best_fcp():
+    param_grid = {'n_epochs': [5, 10], 'lr_all': [0.002, 0.005], 'reg_all': [0.4, 0.6]}
+    gridSearch = GridSearch(SVD,param_grid, measure='FCP')
+    gridSearch.evaluate(data)
+    assert gridSearch.best_index_ == 7
+    assert gridSearch.best_params_ == {u'lr_all': 0.005, u'reg_all': 0.6, u'n_epochs': 10}
+    assert (gridSearch.best_score_ - 0.591655912113) < 0.000001 # scores are equal
