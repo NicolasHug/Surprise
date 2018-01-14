Slightly modified AUC computation, which is wrong (see todo)

tests/test_bpr.py
@@ -15,24 +15,23 @@ from surprise.model_selection import LeaveOneOut
 data_file = os.path.join(os.path.dirname(__file__), './u1_ml100k_train')
 data = Dataset.load_from_file(data_file, Reader('ml-100k'))
 
-data = Dataset.load_builtin('ml-100k')
+#data = Dataset.load_builtin('ml-100k')
 data.binarize(threshold=0)
 
 
 def AUC(pred_testset, pred_anti_testset, trainset):
 
     pred_u_anti_testset = defaultdict(list)
-    for u, j, r_uj, hat_x_uj, _ in pred_anti_testset:
+    for u, j, _, hat_x_uj, _ in pred_anti_testset:
         pred_u_anti_testset[u].append(hat_x_uj)
 
-    AUC = 0
-    for u, i, r_ui, hat_x_ui, _ in pred_testset:
-        AUC_u = 0
-        for hat_x_uj in pred_u_anti_testset[u]:
-            AUC_u += (hat_x_ui > hat_x_uj)
-        if pred_u_anti_testset[u]:
-            AUC_u /= len(pred_u_anti_testset[u])
+    print(trainset.n_users)
+    print(len(pred_testset))
 
+    AUC = 0
+    for u, _, _, hat_x_ui, _ in pred_testset:
+        AUC_u = np.mean([(hat_x_ui > hat_x_uj) for hat_x_uj in
+                         pred_u_anti_testset[u]])
         AUC += AUC_u
     AUC /= trainset.n_users
 
@@ -48,7 +47,7 @@ def test_zob():
 
     mf = BPRMF(n_epochs=100000)
     mf = Dumb()
-    loo = LeaveOneOut()
+    loo = LeaveOneOut(min_n_ratings=0)
     for trainset, testset in loo.split(data):
 
         mf.fit(trainset)