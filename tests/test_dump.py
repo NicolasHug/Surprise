5a22369525be5a7dfd8fb97387a62efed3b800eb
Nicolas Hug <contact@nicolas-hug.com>
2022-09-23 17:35:32 +0100

    Fix dump issue

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -4,11 +4,42 @@
 import random
 import tempfile
 
-from surprise import BaselineOnly, dump
+import pytest
+
+from surprise import (
+    BaselineOnly,
+    CoClustering,
+    dump,
+    KNNBaseline,
+    KNNBasic,
+    KNNWithMeans,
+    KNNWithZScore,
+    NMF,
+    NormalPredictor,
+    SlopeOne,
+    SVD,
+    SVDpp,
+)
 from surprise.model_selection import PredefinedKFold
 
 
-def test_dump(u1_ml100k):
+@pytest.mark.parametrize(
+    "algo",
+    (
+        NormalPredictor(),
+        BaselineOnly(),
+        KNNBasic(),
+        KNNWithMeans(),
+        KNNBaseline(),
+        SVD(),
+        SVDpp(),
+        NMF(),
+        SlopeOne(),
+        CoClustering(),
+        KNNWithZScore(),
+    ),
+)
+def test_dump(algo, u1_ml100k):
     """Train an algorithm, compute its predictions then dump them.
     Ensure that the predictions that are loaded back are the correct ones, and
     that the predictions of the dumped algorithm are also equal to the other
@@ -18,7 +49,10 @@ def test_dump(u1_ml100k):
 
     trainset, testset = next(PredefinedKFold().split(u1_ml100k))
 
-    algo = BaselineOnly()
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        dump.dump(tmp_file.name, algo=algo)
+        dump.load(tmp_file.name)
+
     algo.fit(trainset)
     predictions = algo.test(testset)
 
@@ -26,9 +60,11 @@ def test_dump(u1_ml100k):
         dump.dump(tmp_file.name, predictions, algo)
         predictions_dumped, algo_dumped = dump.load(tmp_file.name)
 
-        predictions_algo_dumped = algo_dumped.test(testset)
         assert predictions == predictions_dumped
-        assert predictions == predictions_algo_dumped
+
+        predictions_algo_dumped = algo_dumped.test(testset)
+        if not isinstance(algo, NormalPredictor):  # predictions are random
+            assert predictions == predictions_algo_dumped
 
 
 def test_dump_nothing():
