19711b2d076cbcdaf6a139f41cc16b3c2f4e94b6
abhi8893 <bhatiaabhishek8893@gmail.com>
2024-05-03 15:12:08 +0530

    create minimal required setup.py, and transfer most conf to pyproject.toml

--- setup.py
+++ setup.py
@@ -60,38 +60,17 @@ In the mean time, upload to conda:
 Then, maybe, celebrate.
 """
 
-from setuptools import dist  # Install numpy right now
+from setuptools import dist
+import numpy as np
 
-dist.Distribution().fetch_build_eggs(["numpy>=1.17.3"])
-
-try:
-    import numpy as np
-except ImportError:
-    exit("Please install numpy>=1.17.3 first.")
-
-try:
-    from Cython.Build import cythonize
-    from Cython.Distutils import build_ext
-except ImportError:
-    USE_CYTHON = False
-else:
-    USE_CYTHON = True
-
-__version__ = "1.1.3"
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from README.md
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-
-# get the dependencies and installs
-with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
-    install_requires = [line.strip() for line in f.read().split("\n")]
-
 cmdclass = {}
 
-ext = ".pyx" if USE_CYTHON else ".c"
+ext = ".pyx"
 
 extensions = [
     Extension(
@@ -121,47 +100,19 @@ extensions = [
     ),
 ]
 
-if USE_CYTHON:
-    # See https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
-    extensions = cythonize(
-        extensions,
-        compiler_directives={
-            "language_level": 3,
-            "boundscheck": False,
-            "wraparound": False,
-            "initializedcheck": False,
-            "nonecheck": False,
-        },
-    )
-    cmdclass.update({"build_ext": build_ext})
+extensions = cythonize(
+    extensions,
+    compiler_directives={
+        "language_level": 3,
+        "boundscheck": False,
+        "wraparound": False,
+        "initializedcheck": False,
+        "nonecheck": False,
+    },
+)
+cmdclass.update({"build_ext": build_ext})
 
 setup(
-    name="scikit-surprise",
-    author="Nicolas Hug",
-    author_email="contact@nicolas-hug.com",
-    description=("An easy-to-use library for recommender systems."),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    version=__version__,
-    url="https://surpriselib.com",
-    license="GPLv3+",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    keywords="recommender recommendation system",
-    packages=find_packages(exclude=["tests*"]),
-    python_requires=">=3.8",
-    include_package_data=True,
     ext_modules=extensions,
-    cmdclass=cmdclass,
-    install_requires=install_requires,
-    entry_points={"console_scripts": ["surprise = surprise.__main__:main"]},
+    cmdclass=cmdclass
 )
